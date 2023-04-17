a4d2ec50b9ea42fbec6cdc67ac640f70ad3e2913
yiwangzhifeng <1442050398@qq.com>
2023-04-17 19:50:58 +0800

    1

--- app/src/main/java/com/example/tran/MainActivity.java
+++ app/src/main/java/com/example/tran/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.tran;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        EditText editText=(EditText) findViewById(R.id.accout);
+//        editText.setOnEditorActionListener(new TextView.OnEditorActionListener(){
+//            @Override
+//            public boolean onEditorAction(TextView v, int actionId, KeyEvent event){
+//                if(actionId== EditorInfo.IME_ACTION_NEXT)return true;
+//                return false;
+//            }
+//        });
     }
 }
