7eea6ad6a9b31c7bb40c695bae604f08de83a9d9
yiwangzhifeng <1442050398@qq.com>
2023-04-23 15:17:20 +0800

    1

--- app/src/main/java/com/example/tran/LoginActivity.java
+++ app/src/main/java/com/example/tran/LoginActivity.java
@@ -6,12 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity2 extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_login);
     }
     public void go(View v){
         Intent intent = new Intent(this, MainActivity.class);
