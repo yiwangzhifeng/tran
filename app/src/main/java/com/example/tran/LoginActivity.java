91e40c1fa049300652bef9409b418efe489d0937
yiwangzhifeng <1442050398@qq.com>
2023-04-25 20:52:13 +0800

    1

--- app/src/main/java/com/example/tran/LoginActivity.java
+++ app/src/main/java/com/example/tran/LoginActivity.java
@@ -5,16 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.SpannableString;
+import android.text.Spanned;
 import android.text.TextWatcher;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText AET;
     private EditText PET;
     private boolean fA=false;
-
+    private TextView textView_sign_in;
     private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +43,27 @@ public class LoginActivity extends AppCompatActivity {
                 button.setEnabled(AET.getText().length() == 11);
             }
         });
+        textView_sign_in=findViewById(R.id.sign_in_text);
+
+        String text = "启动一个Activity";
+
+        SpannableString spannableString = new SpannableString(text);
+
+        spannableString.setSpan(new ClickableSpan() {
+
+            @Override
+            public void onClick(View widget) {
+                //Intent 启动一个Activity
+                Intent intent = new Intent(this, SignInActivity.class);
+                //跳转
+                startActivity(intent);
+            }
+        }, 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        textView_sign_in.setText(spannableString);
+
+        textView_sign_in.setMovementMethod(LinkMovementMethod.getInstance());// 可以点击
+
     }
     public void go(View v){
         Intent intent = new Intent(this, MainActivity.class);
